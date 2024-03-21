8191c941b0691178da3f86865b465e78d07a064e
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2024-03-21 09:29:46 -0300

    feat: organizing to work with routes

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <header className="bg-white w-full h-20" />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
